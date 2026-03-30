a42c056e2330a50175b32132480999c2d5c5a61a
computerbox <computerbox124@gmail.com>
2026-03-30 23:37:16 +0400

    Part 2 completed - Dynamic Alerting added

--- src/main/omnihome/reactive/devices/SmartAlarm.java
+++ src/main/omnihome/reactive/devices/SmartAlarm.java
@@ -1,13 +1,41 @@
 package main.omnihome.reactive.devices;
 
 import main.omnihome.reactive.observer.Observer;
+import main.omnihome.reactive.strategy.AlertStrategy;
+import main.omnihome.reactive.strategy.LoudSirenStrategy;
+import main.omnihome.reactive.strategy.SilentPushStrategy;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class SmartAlarm implements Observer {
-    @Override
-    public void update() {
-        System.out.println("SmartAlarm received motion event.!");
+    private final Map<String, AlertStrategy> strategyRegistry;
+    private AlertStrategy currentStrategy;
+    private boolean armed;
 
+    public SmartAlarm() {
+        this.strategyRegistry = new HashMap<>();
+        strategyRegistry.put("SILENT", new SilentPushStrategy());
+        strategyRegistry.put("SIREN", new LoudSirenStrategy());
+        this.currentStrategy = strategyRegistry.get("SILENT");
+        this.armed = false;
     }
 
+    @Override
+    public void update() {
+        System.out.println("SmartAlarm received motion event.");
+        currentStrategy.executeAlert();
+    }
+
+    public void setAlertStrategy(String mode) {
+        AlertStrategy selectedStrategy = strategyRegistry.get(mode);
+
+        if (selectedStrategy == null) {
+            throw new IllegalArgumentException("Unknown alert strategy: " + mode);
+        }
+
+        currentStrategy = selectedStrategy;
+        System.out.println("SmartAlarm strategy changed to " + mode + ".");
+    }
 
 }
---
+++ src/main/omnihome/reactive/strategy/LoudSirenStrategy.java
@@ -0,0 +1,11 @@
+package main.omnihome.reactive.strategy;
+
+
+public class LoudSirenStrategy implements AlertStrategy {
+    @Override
+    public void executeAlert() {
+        System.out.println("SOUNDING 120dB SIREN!");
+    }
+
+
+}
